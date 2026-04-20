77d3689266188d8d4d5f7efca4c53971f94c41a9
akoudeif-umich <akoudeif@umich.edu>
2026-04-19 20:08:24 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #prices[i] = neetcoin on the ith day 
+        #choose a day to buy then sell in the future
+        maxp = 0
+        l , r = 0, 0
+
+        while r < len(prices):
+            maxp = max(maxp, prices[r] - prices[l])
+
+            if prices[r] < prices[l]:
+                l = r
+            r += 1
+
+        return maxp
